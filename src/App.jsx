1b8394e68be83da798e72440b9f06097bbab7073
ysf-kazi <yusufkazi08@outlook.com>
2026-09-14 19:19:09 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1805,7 +1805,7 @@ function App() {
 
                             <div className="selcom-qr-box">
                               <img
-                                src="/src/assets/selcom-qr.jpg"
+                                src="./public/selcom_qr.jpg"
                                 alt="Selcom Lipa Namba QR Code"
                               />
                             </div>
